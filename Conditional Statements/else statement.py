56d8f224cd77b8b1ae3953ce48b70ee799406f4b
kipmyk <kipmyk@gmail.com>
2025-06-02 20:49:01 +0300

    Chore: added else statement example

---
+++ Conditional Statements/else statement.py
@@ -0,0 +1,8 @@
+# else: Executes if none of the above conditions are True
+
+age = int(input("How old are you?: "))
+
+if age >= 18:
+    print("You are now signed up!")
+else:
+    print("You must be 10+ to sign up!")
