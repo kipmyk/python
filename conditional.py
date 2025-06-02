49c56e323b26f348c44de09024fa4e3eda607b4e
kipmyk <kipmyk@gmail.com>
2025-06-02 16:21:41 +0300

    conditionals in python starting with if statements

---
+++ conditional.py
@@ -0,0 +1,19 @@
+# if statements
+is_student = True
+for_sale = True
+is_online = True
+
+if is_student:
+    print("You are a student")
+else:
+    print("You are not a student")
+
+if for_sale:
+    print("This code is for sale")
+else:
+    print("This code is not available")
+
+if is_online:
+    print("You are online")
+else:
+    print("You are offline")
