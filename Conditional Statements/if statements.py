5130e1fee636e2317f7a987ec393f3b0f0ad5ec8
kipmyk <kipmyk@gmail.com>
2025-06-02 20:48:38 +0300

    Chore: added if statement example

---
+++ Conditional Statements/if statements.py
@@ -0,0 +1,9 @@
+# if: Runs a block of code only if a specific condition is True
+# else: Runs an alternative block if the condition is False
+
+age = int(input("How old are you?: "))
+
+if age >= 18:
+    print("You are now signed up!")
+else:
+    print("You must be 10+ to sign up!")
