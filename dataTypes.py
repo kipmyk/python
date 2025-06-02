dedbf6b8d1c1bb0657028c371b62ba69de151350
kipmyk <kipmyk@gmail.com>
2025-06-02 16:20:47 +0300

    Data Types

---
+++ dataTypes.py
@@ -0,0 +1,31 @@
+# A variable = A container for a value, and there, Strings, integers, floats, and booleans
+# Variables behave as if it was the value it contain
+# strings, a series of characters
+first_name = "Mike"
+food = "pizza"
+email = "e@kipmyk.co.ke"
+
+# Integers
+age = 25
+quantities = 3
+num_of_students = 30
+
+# float
+price = 10.99
+gpa = 3.2
+distance = 5.5
+
+# Boolean
+
+is_student = True
+
+# Displaying variables
+
+print(f"Hello {first_name}, it seems you like {food}, and your email is {email}")
+print(f"You are {age} years old")
+print(f"You are buying {quantities} items")
+print(f"My uni class had {num_of_students}")
+print(f"The price is ${price}")
+print(f"My GPA is {gpa}")
+print(f"You ran {distance} km")
+print(f"Are you a student?: {is_student}")
