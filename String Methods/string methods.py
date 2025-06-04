5a445066d88927f0ff7f3f3f0c8c416caf2e72ca
kipmyk <kipmyk@gmail.com>
2025-06-04 11:55:40 +0300

    Added: Strings methods

---
+++ String Methods/string methods.py
@@ -0,0 +1,23 @@
+# name = input("Enter your full name: ")
+from dataclasses import replace
+
+# phone_number = input("Enter your phone number: ")
+
+# name = len(name)
+
+# name = name.find("M") # finds the first occurrence of input
+# name = name.rfind("o") # finds the last occurrence of input
+
+# name = name.capitalize()
+# name = name.upper()
+
+# name = name.lower()
+
+# name = name.isdigit()
+# name = name.isalpha()
+# print(name)
+# phone = phone_number.count("-")
+# space_replace = phone_number.replace("-"," ")
+# print(space_replace)
+
+print(help(str))
