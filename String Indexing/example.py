605c98cbd4245d69e018d6b7257600d588712944
kipmyk <kipmyk@gmail.com>
2025-06-05 07:40:29 +0300

    Added: string indexing ✂️ examples

---
+++ String Indexing/example.py
@@ -0,0 +1,9 @@
+# Exercise 1: print the last 4 digits
+credit_card_number = "1234-5678-9012-3556"
+last_digits = credit_card_number[-4:]
+print(f"XXXX-XXXX-XXX-{last_digits}")
+
+# Exercise 2: reverse characters in credit_card_number
+credit_card_number = "1234-5678-9012-3556"
+credit_card_number = credit_card_number[::-1]
+print(credit_card_number)
