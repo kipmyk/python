5052ce3849899df01f97995c4075c39effe21e7d
kipmyk <kipmyk@gmail.com>
2025-06-09 11:43:41 +0300

    Added: For Loops  🔁

---
+++ For loops/forloops.py
@@ -0,0 +1,18 @@
+# for loops 🔁 - it executes a block of code a fixed number of times.
+#              - You can iterate over a range, string, sequence, etc.
+
+
+for counter in range (1,11,2):
+    print(counter)
+print("Done 🔄!")
+
+credit_card_number = "1234-5678-9101-4123"
+
+for y in credit_card_number:
+    print(y)
+
+for w in range (1,21):
+    if(y==13):
+        break
+    else:
+        print(w)
